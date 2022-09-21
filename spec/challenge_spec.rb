054eadd63dc5b04df2ab5a53d386e0fd97f2a406
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-09-21 15:36:12 +0100

    Pass all tests for challenge

--- spec/challenge_spec.rb
+++ spec/challenge_spec.rb
@@ -1,8 +1,11 @@
 RSpec.describe "doubles challenge" do
   it "creates a sophisticated double" do
-    task_list = # ...
-    task = # ...
-
+    task_list = double :fake_task_list
+    task = double :fake_task
+    allow(task_list).to receive(:add).with(task)
+    allow(task_list).to receive(:list).and_return [task]
+    allow(task_list).to receive(:count).and_return 1
+    allow(task_list).to receive(:clear).and_return :success
     # Don't edit below
     task_list.add(task)
     expect(task_list.list).to eq [task]
